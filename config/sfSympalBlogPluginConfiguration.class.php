efd4a06168a8b5b6aa9e605c1e2d3de81754e4fa
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-22 05:57:01 -0500

    [1.0] sfSympalBlogPlugin: Fixes configuration to use static dependencies
    git-svn-id: http://svn.symfony-project.com/plugins/sfSympalBlogPlugin/trunk@16500 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/sfSympalBlogPluginConfiguration.class.php
+++ config/sfSympalBlogPluginConfiguration.class.php
@@ -1,7 +1,7 @@
 <?php
 class sfSympalBlogPluginConfiguration extends sfPluginConfiguration
 {
-  public 
+  public static
     $dependencies = array(
       'sfSympalPlugin',
       'sfSympalSecurityPlugin',
