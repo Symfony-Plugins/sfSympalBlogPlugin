ee29169d40dceb49bc72d4e5692f9b601e7f68f1
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-29 14:44:48 -0500

    [1.0] sfSympalBlogPlugin: Updating blog plugin to allow inline editing of content properties
    git-svn-id: http://svn.symfony-project.com/plugins/sfSympalBlogPlugin/trunk@16718 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sympal_blog/templates/_view.php
+++ modules/sympal_blog/templates/_view.php
@@ -4,16 +4,21 @@
 
 <div id="sympal_blog">
   <div id="view">
-    <h2><?php echo $content->getHeaderTitle() ?></h2>
+    <h2><?php echo get_sympal_column_content_slot($content, 'title', 'Text', 'span') ?></h2>
 
     <?php echo image_tag($content->CreatedBy->Profile->getGravatarUrl(), 'align=right') ?>
 
     <p>
       <strong>
         Posted by <?php echo link_to($content->CreatedBy->username, $content->CreatedBy->Profile->Content->getRoute()) ?> on 
-        <?php echo date('m/d/Y h:i:s', strtotime($content->created_at)) ?>
+        <?php echo get_sympal_column_content_slot($content, 'date_published', 'Text', 'span') ?>
       </strong>
     </p>
+
+    <?php if (sfSympalToolkit::isEditMode()): ?>
+      <?php echo get_sympal_column_content_slot($content, 'teaser', 'MultiLineText') ?>
+      <hr/>
+    <?php endif; ?>
 
     <?php echo get_sympal_content_slot($content, 'body', 'Markdown') ?>
   </div>
